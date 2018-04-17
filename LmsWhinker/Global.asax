11ef5c56e69afa8cadc910735493b007e9d27606
engmanuelricardoperez <manuelricardo.perez@hotmail.com>
2018-04-17 11:22:36 -0500

    Agregar archivos de proyecto.

---
+++ LmsWhinker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LmsWhinker.MvcApplication" Language="C#" %>
---
+++ LmsWhinker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LmsWhinker.Startup))]
+namespace LmsWhinker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
